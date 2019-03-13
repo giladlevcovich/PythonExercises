76cb3bfa048a4d6055c7c86148f073c003c69368
Gilad Levcovich <gilad.levco@gmai.com>
2019-03-13 12:12:03 +0200

    check secret code

--- ex1.py
+++ ex1.py
@@ -1,3 +1,8 @@
-print("hello")
-x=3
-print(x)
+secret_code='1234'
+money='1000'
+
+def check_secret_code(entered_secret_code):
+    if entered_secret_code==secret_code:
+        return True
+    else:
+        return False

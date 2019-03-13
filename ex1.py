38534e545258238235e2f6f776112f3852842370
Gilad Levcovich <gilad.levco@gmai.com>
2019-03-13 12:49:57 +0200

    new code func

--- ex1.py
+++ ex1.py
@@ -6,3 +6,10 @@ def check_secret_code(entered_secret_code):
         return True
     else:
         return False
+
+def update_secret_code():
+    new_secret_code= input("Enter new secret code")
+    while not new_secret_code.isdigit():
+        print("You didn't enter a number, enter again")
+        new_secret_code=input()
+    secret_code=new_secret_code

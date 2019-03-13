ba640aabf6cf55558523cf02b8cbe3e7557ffd95
Gilad Levcovich <gilad.levco@gmai.com>
2019-03-13 21:34:52 +0200

    finished ex2

--- ex2.py
+++ ex2.py
@@ -1,1 +1,20 @@
-print("ex2");
+sum=0
+print("Which Question?")
+question=input()
+
+if question=='1':
+    while True:
+        list_input = input("Enter number until stop:")
+        if list_input == 'stop':
+            break
+        else:
+            sum = int(sum) + int(list_input)
+    print("The sum of the list is:" + str(sum))
+
+if question=='2':
+    list_input = input("Enter numbers with comma:")
+    withoutcomma=list_input.split(",")
+    for num in withoutcomma:
+        if num!=',':
+            sum=sum+int(num)
+print("The sum of the list is:" + str(sum))
